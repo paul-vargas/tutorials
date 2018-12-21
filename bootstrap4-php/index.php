106fb2937b2a877c49797034cdcccacd59d23e4a
Paul Vargas <github@paulvargas.org>
2018-12-21 12:50:12 -0600

    Added php example.

---
+++ bootstrap4-php/index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+        <title>Hello, world!</title>
+        <style>
+            .row:hover {
+                background-color: #FAFAFA;
+            }
+        </style>        
+    </head>
+    <body>
+        <div class="container-fluid">
+            <h1>Hello, world!</h1>
+            ...
+
+
+            <div class="row">
+                <div class="col-sm d-none d-sm-block">
+                    <span class="text-center align-middle">Text</span>
+                    <button type="button" class="btn btn-light btn-sm float-right">-</button>
+                </div>
+                <div class="col-sm d-none d-sm-block">
+                    <span class="text-center align-middle">Text</span>
+                    <button type="button" class="btn btn-light btn-sm float-right">-</button>
+                </div>
+                <div class="col-sm d-none d-sm-block">
+                    <span class="text-center align-middle">Text</span>
+                    <button type="button" class="btn btn-light btn-sm float-right">-</button>
+                </div>
+            </div>              
+
+            <div class="row">
+                <div class="col-sm">
+                    One of three columns
+                </div>
+                <div class="col-sm">
+                    One of three columns
+                </div>
+                <div class="col-sm">
+                    One of three columns
+                </div>
+            </div>  
+            <div class="row d-block d-sm-none border-bottom border-light">
+            </div>   
+            <div class="row">
+                <div class="col-sm">
+                    One of three columns
+                </div>
+                <div class="col-sm">
+                    One of three columns
+                </div>
+                <div class="col-sm">
+                    One of three columns
+                </div>
+            </div>  
+            <div class="row">
+                <div class="col-sm">
+                    One of three columns
+                </div>
+                <div class="col-sm">
+                    One of three columns
+                </div>
+                <div class="col-sm">
+                    One of three columns
+                </div>
+            </div>  
+
+        </div>
+
+
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/esm/popper.min.js" integrity="sha256-T0gPN+ySsI9ixTd/1ciLl2gjdLJLfECKvkQjJn98lOs=" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
