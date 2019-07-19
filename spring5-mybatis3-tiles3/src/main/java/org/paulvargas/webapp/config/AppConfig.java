07819a3de34089c9fe4b340d9e8368f00f8b6879
Paul Vargas <github@paulvargas.org>
2019-07-19 14:48:53 -0500

    Added Hikari.

--- spring5-mybatis3-tiles3/src/main/java/org/paulvargas/webapp/config/AppConfig.java
+++ spring5-mybatis3-tiles3/src/main/java/org/paulvargas/webapp/config/AppConfig.java
@@ -1,5 +1,29 @@
 package org.paulvargas.webapp.config;
 
-public class AppConfig {
+import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+public class AppConfig {
+	@Bean(name = "firstDataSource")
+	@ConfigurationProperties(prefix = "datasources.first")
+	public DataSource dataSourceOne() {
+		return new HikariDataSource();
+	}
+	
+	@Bean(name = "firstSqlSessionFactory")
+	public SqlSessionFactory firstSqlSessionFactory(@Qualifier("firstDataSource") final DataSource dataSource)
+			throws Exception {
+		final SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(dataSource);
+		final SqlSessionFactory factory = factoryBean.getObject();
+		factory.getConfiguration().setMapUnderscoreToCamelCase(true);
+		return factory;
+	}	
 }
